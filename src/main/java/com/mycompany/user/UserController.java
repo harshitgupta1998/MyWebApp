e031fb3408b5fac2c644132dbe707db805392674
Harhsit Gupta <harshit@therapprove.com>
2025-06-29 17:09:38 -0400

    Updated code

---
+++ src/main/java/com/mycompany/user/UserController.java
@@ -0,0 +1,23 @@
+package com.mycompany.user;
+
+import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.swing.*;
+import java.util.List;
+
+@Controller
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public String showUserList(Model model) {
+        List<User> listUsers = userService.listAll();
+
+        model.addAttribute("listUsers",listUsers);
+        return "users";
+    }
+}
